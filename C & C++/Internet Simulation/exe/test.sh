51bb6abe9f85ea5d92d016b2e22153a3db3d8318
Tyler Reed <tylernathanreed@gmail.com>
2014-05-10 13:38:01 -0500

    Renamed test.sh to test1.sh

--- C & C++/Internet Simulation/exe/test.sh
+++
@@ -1,2 +0,0 @@
-Node -i 0 -l 20 -m "The Simplest thing in the world is SSS" -d 1 -n 1 1 -D 3&
-Node -i 1 -l 20 -n 1 0 -D 3&
---
+++ C & C++/Internet Simulation/exe/test1.sh
@@ -0,0 +1,2 @@
+Node -i 0 -l 20 -m "This is a Message" -d 1 -n 1 1 -D 3&
+Node -i 1 -l 20 -n 1 0 -D 3&
